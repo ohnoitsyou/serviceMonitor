cde42b54deaef49e4e33483154ae6436e8a85d1c
David Young <ohnoitsyou7043@gmail.com>
2013-12-22 21:34:25 +0000

    adding templates, probably not the best named templates but templates at the moment, removed most of the debug output

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 // Server
 var express = require('express')
+  , hbs = require('express-hbs')
   , app = express()
   , http = require('http')
   , server = http.createServer(app)
@@ -9,36 +10,42 @@ var express = require('express')
 
 server.listen(3000);
 
+app.engine('hbs', hbs.express3({
+  partialsDir: __dirname + '/views/partials',
+  contentHelperName : 'content',
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
-  var resString = "";
-  for( client in io.sockets.sockets) {
-  clientRef = io.sockets.socket(client);
-  clientName   = "";
-  clientStatus = "";
-  clientRef.get('name'  , function(err, name) {
-    clientName = name;
-  });
-  clientRef.get('status', function(err,srvStatus) {
-    clientStatus = srvStatus;
-  });
-  resString += '<div class="statusbox"><span class="clientName">' + clientName + '</span><span class="clientStatus">'+ clientStatus + '</span></div>';
+  var services = [];
+  for(client in io.sockets.sockets) {
+    clientRef = io.sockets.socket(client);
+    service = {};
+    clientRef.get('name', function(err, name) {
+      service.name = name;
+    });
+    clientRef.get('status', function(err, srvstatus) {
+      service.srvstatus = srvstatus;
+    });
+    services.push(service);
   }
-  res.send(resString);
-  res.end();
+  console.log(util.inspect(services)); 
+  res.render('index', {'title': 'testing', 'service' : services}); 
 });
 
 io.sockets.on('connection', function(socket) {
   console.log('New client');
   socket.emit('name', function(name) {
     socket.set('name',name);
-    console.log('name: ' + name);
+    //console.log('name: ' + name);
   }); 
   setTimeout(updateStatus(socket), 1000)
 });
 function updateStatus(socket) {
   socket.emit('status', function(srvstatus) {
     socket.set('status', srvstatus);
-    console.log('status: ' + srvstatus);
+    //console.log('status: ' + srvstatus);
   });
 }
 setInterval(function() {
